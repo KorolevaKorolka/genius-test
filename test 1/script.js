d6d1327fd2042ec1296f9dbc2fdec606439929af
KorolevaKorolka <76254153+KorolevaKorolka@users.noreply.github.com>
2023-11-16 20:19:46 +0200

    Add files via upload

---
+++ test 1/script.js
@@ -0,0 +1,11 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  console.log(args.length);
+};
+
+argumentsLength(1, 2, 3); // 3
+argumentsLength(5);
+argumentsLength({}, null, "3");
